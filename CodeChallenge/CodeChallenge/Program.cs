48e67d50b516a1271ffe7fd43850721b8dfea099
Bob Banks <bob.banks@gmail.com>
2016-09-27 22:25:17 -0400

    Initial commit

---
+++ CodeChallenge/CodeChallenge/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace CodeChallenge {
+    internal class Program {
+        private static void Main(string[] args) {
+
+            ProcessViaCharacterEnumeration("(id,created,employee(id,firstname,employeeType(id), lastname),location)");
+            Console.ReadKey();
+        }
+
+        private static void ProcessViaCharacterEnumeration(string data) {
+            var dataArray = data.ToCharArray();
+            var indentLevel = 0;
+
+            foreach (var character in dataArray) {
+                switch (character) {
+                    case '(':
+                        indentLevel++;
+                        if (indentLevel > 1) {
+                            Console.WriteLine();
+                        }
+                        Console.Write(new string('-',indentLevel - 1));
+                        if (indentLevel > 1) {
+                            Console.Write(' ');
+                        }
+                        break;
+                    case ')':
+                        indentLevel--;
+                        break;
+                    case ',':
+                        Console.WriteLine();
+                        Console.Write(new string('-',indentLevel - 1));
+                        if (indentLevel > 1) {
+                            Console.Write(' ');
+                        }
+                        break;
+                    case ' ':
+                        break;
+                    default:
+                        Console.Write(character);
+                        break;
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
